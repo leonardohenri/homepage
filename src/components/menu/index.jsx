2f609249bc38fafc8b00edcdb5a9099627b2be21
leonardo <leonardohenri2015@outlook.com>
2023-01-12 14:01:12 -0300

    menu responsive

--- src/components/menu/index.jsx
+++ src/components/menu/index.jsx
@@ -1,15 +1,22 @@
 import "./styles.css"
+import img from "../../images/icon-menu.svg"
+import { openNav, closeNav } from "./script"
+import close from "../../images/icon-menu-close.svg"
 
 export const Menu = () =>{
     return(
         <nav >
-            <ul className="menu">
-             <li><a href="/">Home</a></li>
-             <li><a href="/">new</a></li> 
-             <li><a href="/">popular</a></li>
-             <li><a href="/">trending</a></li>
-             <li><a href="/">categories</a></li>  
-            </ul>
+            <div id="buttonMenu" onClick={openNav}><img src={img} alt="" /> </div>
+            <div id="menu">
+                <ul >
+                <div id="buttonClose" onClick={closeNav}><img src={close} alt="" /></div>
+                 <li><a href="/">Home</a></li>
+                 <li><a href="/">new</a></li>
+                 <li><a href="/">popular</a></li>
+                 <li><a href="/">trending</a></li>
+                 <li><a href="/">categories</a></li>
+                </ul>
+            </div>
         </nav>
     );
 }
---
+++ src/components/menu/script.js
@@ -0,0 +1,11 @@
+
+ 
+   export function openNav(){
+    document.getElementById('menu').style.display="block"
+    document.getElementById('buttonMenu').style.display="none"
+   }
+   export function closeNav(){
+    document.getElementById('menu').style.display="none"
+    document.getElementById('buttonMenu').style.display="block"
+   }
+
